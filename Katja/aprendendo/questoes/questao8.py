95e8a6c2e0a5adcfe64d6475c64d81309ddec2c8
KatjaMateus <145811394+KatjaMateus@users.noreply.github.com>
2023-10-15 16:32:53 -0300

    questao8.py

--- Katja/aprendendo/questoes/questao8.py
+++ Katja/aprendendo/questoes/questao8.py
@@ -6,5 +6,5 @@ if produto1 < produto2 and produto1 < produto3:
     print("Compre o produto1")
 elif produto2 < produto1 and produto2 < produto3:
     print("Compre o produto2")
-elif produto3 < produto1 and produto3 < produto2:
+else:
     print("Compre o produto3")
