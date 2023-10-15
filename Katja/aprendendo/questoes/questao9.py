db66f56d05464c635e0f30d851449e48154844b6
KatjaMateus <145811394+KatjaMateus@users.noreply.github.com>
2023-10-15 17:46:39 -0300

    questao9.py

--- Katja/aprendendo/questoes/questao9.py
+++ Katja/aprendendo/questoes/questao9.py
@@ -2,11 +2,20 @@ numero1 = int(input("numero1: "))
 numero2 = int(input("numero2: "))
 numero3 = int(input("numero3: "))
 
-if numero1 < numero2 and numero1 < numero3 and numero2 > numero1 and numero2 < numero3 and numero3 > numero1 and numero3 > numero2:
-    print(numero3, numero2, numero1)
+if numero1> numero2 and numero1> numero3 :
+    if numero2>numero3:
+        print(numero1, numero2, numero3)
+    else:
+        print(numero1, numero3, numero2)
 
-elif numero2 < numero1 and numero2 < numero3 and numero1 > numero2 and numero1 < numero3 and numero3 > numero1 and numero3 > numero2:
-    print(numero3, numero1, numero2)
+elif numero2 > numero1 and numero2 > numero3:
+    if numero1>numero3:
+        print(numero2, numero1, numero3)
+    else:
+        print(numero2, numero3, numero1)
 
-elif numero3 < numero1 and numero3 < numero2 and numero2 > numero3 and numero2 > numero1 and numero1 < numero2 and numero1 > numero3:
-    print(numero2, numero1, numero3)
+else:
+    if numero2>numero1:
+        print(numero3, numero2, numero1)
+    else:
+        print(numero3, numero1, numero2)
