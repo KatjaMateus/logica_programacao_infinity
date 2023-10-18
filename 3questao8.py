8ab764135a25966f31c29b7f424d0dd4afc9db74
KatjaMateus <145811394+KatjaMateus@users.noreply.github.com>
2023-10-17 23:16:58 -0300

    Add files via upload

---
+++ 3questao8.py
@@ -0,0 +1,6 @@
+cont = 0
+palavra = "kampaaja"
+for letra in palavra:
+    if letra in "aeiou":
+        cont += 1
+        print(cont)
---
+++ 3questao9.py
@@ -0,0 +1,4 @@
+palavra = "kampaaja"
+for letra in palavra:
+    if letra in "aeiou":
+        print(len(letra))
