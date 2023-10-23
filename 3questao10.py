10a243347b93b33db48074e459f73ea64326ee03
KatjaMateus <145811394+KatjaMateus@users.noreply.github.com>
2023-10-23 19:38:25 -0300

    3questao10.py

--- 3questao10.py
+++ 3questao10.py
@@ -9,3 +9,23 @@ if inverso == junto:
     print("Temos um palíndromo")
 else:
     print("Não é um palíndromo: ")
+
+'''frase = input('Digite uma palavra: ').strip().upper()
+palavras = frase.replace(' ', '')
+
+--------------
+OPÇÃO 1:
+ultimo_indice = len(palavras)-1
+for indice in range(ultimo_indice, -1, -1):
+    # para acessar a posição: lista[indice]
+    print(palavras[indice])
+
+OPÇÃO 2:
+inverso = palavras[::-1]
+--------------
+
+if palavras == inverso:
+    print('É palíndromo')
+else:
+    print(f'Não é palíndromo. O inverso é {inverso}.')
+'''
