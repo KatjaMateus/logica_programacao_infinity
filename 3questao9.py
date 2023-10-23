e1e723cdb59a353f5b63afc2216abd4ff4c65c48
KatjaMateus <145811394+KatjaMateus@users.noreply.github.com>
2023-10-23 19:37:35 -0300

    3questao9.py

--- 3questao9.py
+++ 3questao9.py
@@ -4,3 +4,13 @@ for letra in palavra:
     if letra not in "aeiou":
         cont += 1
 print(cont)
+
+'''cont = 0
+palavra = input('Digite uma palavra: ')
+
+for letra in palavra:
+    if letra in 'aeiou':
+        pass
+    else:
+        cont += 1
+print(f'Há {cont} consoantes em {palavra}')'''
