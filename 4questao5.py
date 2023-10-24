7c23625373774da7918fd058ea9a349abf689de4
KatjaMateus <145811394+KatjaMateus@users.noreply.github.com>
2023-10-24 20:41:56 -0300

    4questao5.py

--- 4questao5.py
+++ 4questao5.py
@@ -7,11 +7,14 @@ taxaB = float(input("Digite a taxa de crescimento da pop B: "))
 tAcalc = (taxaA / 100) + 1
 tBcalc = (taxaB / 100) + 1
 
-    
+if popA > popB or taxaA < taxaB:
+    print("Digite números novamente. População A deve ser menor que B, e taxaA deve ser maior que taxaB")
+    popA = int(input("Digite número de habitantes do país A: "))  
+    popB = int(input("Digite número de habitantes do país B: "))
+    taxaA = float(input("Digite a taxa de crescimento da pop A: "))
+    taxaB = float(input("Digite a taxa de crescimento da pop B: "))
 while popA < popB and taxaA > taxaB:
     popA *= tAcalc
     popB *= tBcalc
     ano += 1
-    print(f"Levarão {ano} anos para população A ultrapassar população B")
-else: 
-    print("Digite números novamente, população do país A deve ser menor do que população do país B, e taxa de crescimento do país A deve ser maior do que do país B")
+print(f"Levarão {ano} anos para população A ultrapassar população B")
