72f88f94e7fab1a9e842660a451f2c33344a3c9a
KatjaMateus <145811394+KatjaMateus@users.noreply.github.com>
2023-10-24 21:08:39 -0300

    4questao10.py

---
+++ 4questao10.py
@@ -0,0 +1,9 @@
+cont = 0
+numero1 = int(input("Digite o primeiro número: "))
+numero2 = int(input("Digite o segundo número: "))
+
+while numero1 < numero2:
+    print(cont)
+    cont += 1
+
+'''loop infinito'''
