6c38dbd9bc275f8dc275be5cbdab929354c4b649
KatjaMateus <145811394+KatjaMateus@users.noreply.github.com>
2023-10-24 21:15:20 -0300

    4questao6.py

--- 4questao6.py
+++ 4questao6.py
@@ -7,3 +7,15 @@ cont = 0
 while cont <= 19:
     cont += 1
     print(cont, end = " ")
+
+'''contador = 0
+while True:
+    contador += 1
+    if contador >= 20:
+        break'''
+
+'''contador = 1
+
+while contador <= numero:
+    print(contador)
+    contador += 1'''
