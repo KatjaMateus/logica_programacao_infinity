659fdb43c7753fd84663832014c7bd59f027cbe6
KatjaMateus <145811394+KatjaMateus@users.noreply.github.com>
2023-10-26 21:27:20 -0300

    4questao6.py

--- 4questao6.py
+++ 4questao6.py
@@ -8,14 +8,31 @@ while cont <= 19:
     cont += 1
     print(cont, end = " ")
 
-'''contador = 0
-while True:
-    contador += 1
-    if contador >= 20:
-        break'''
-
 '''contador = 1
 
 while contador <= numero:
     print(contador)
-    contador += 1'''
+    contador += 1
+
+--------------- OU -------------
+
+contador = 0
+while True:
+    contador += 1
+    if contador >= 20:
+        break
+--------------------------------------
+# Começando com zero
+
+cont = 0            # um número antes do desejado
+while cont < 20:    # cont <= 19
+    cont += 1       # virar o número desejado
+    print(cont)
+
+
+# Começando por um
+
+cont = 1            # já é o número desejado
+while cont <= 20:   # cont < 21
+    print(cont)     # exibindo
+    cont += 1'''
