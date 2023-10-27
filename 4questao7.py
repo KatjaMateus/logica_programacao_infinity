b4c80c23f764d3b26b38bce854c8a5b468320385
KatjaMateus <145811394+KatjaMateus@users.noreply.github.com>
2023-10-26 21:35:16 -0300

    4questao7.py

--- 4questao7.py
+++ 4questao7.py
@@ -1,3 +1,8 @@
-for i in range(5):
-    num = int(input("Digite um número: "))
-print(num)
+maior_numero = cont = 0
+
+while cont < 5:
+    cont += 1
+    num = int(input(f"Informe o {cont}º número: "))
+    if maior_numero < num:
+        maior_numero = num
+print(f"O maior número foi {maior_numero}")
